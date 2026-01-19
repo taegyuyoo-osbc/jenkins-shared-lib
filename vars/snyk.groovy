b6debc49e7455e67bacd43c1bccd34809cfcb661
taegyuyoo-osbc <taegyuyoo@osbc.co.kr>
2026-01-19 13:38:12 +0900

    modify snyk_groovy

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -1,11 +1,24 @@
 def call(Map c = [:]) {
-    withCredentials([string(credentialsId: ${c.snykToken}, variable: 'TOKEN')]) {
-    sh """
-        snyk auth $TOKEN
-        snyk test \
-            --severity-threshold=${c.severity ?: 'high'} \
-            ${c.failOn == false ? '|| true' : ''} \
-            --org=${c.snykOrg} --project-name=${c.projectName}
-    """
+
+    def severity = c.severity ?: 'high'
+    def failOn = c.failOn?.toString()?.toBoolean()
+    def snykOrg = c.snykOrg
+    def projectName = c.projectName
+
+    if (!c.snykToken) {
+        error "snykToken(credentialsId)은 필수입니다"
+    }
+
+    withCredentials([
+        string(credentialsId: c.snykToken, variable: 'TOKEN')
+    ]) {
+        sh """
+            snyk auth \$TOKEN
+            snyk test \
+              --severity-threshold=${severity} \
+              --org=${snykOrg} \
+              --project-name=${projectName} \
+              ${failOn == false ? '|| true' : ''}
+        """
     }
 }
